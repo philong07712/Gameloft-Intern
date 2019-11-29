668289ca9c07649bceadda5c06c06969a05b5bb1
Han <philong07712@gmail.com>
2019-11-29 14:08:27 +0700

    Get data from plist

--- Day8/HelloWorld2/Classes/LoadingScene.cpp
+++ Day8/HelloWorld2/Classes/LoadingScene.cpp
@@ -12,6 +12,19 @@ bool LoadingScene::init()
 		return false;
 	}
 	scheduleUpdate();
+	auto spriteCache = SpriteFrameCache::getInstance();
+	spriteCache->addSpriteFramesWithFile("Robot1.plist", "Robot1.png");
+	auto player = Sprite::create();
+	player->setPosition(100, 100);
+	addChild(player);
+	Vector<SpriteFrame*> animFrames; 
+	animFrames.pushBack(spriteCache->getSpriteFrameByName("character_robot_walk0.png"));
+	animFrames.pushBack(spriteCache->getSpriteFrameByName("character_robot_walk1.png"));
+	animFrames.pushBack(spriteCache->getSpriteFrameByName("character_robot_walk2.png"));
+	animFrames.pushBack(spriteCache->getSpriteFrameByName("character_robot_walk3.png"));
+	auto animation = Animation::createWithSpriteFrames(animFrames, 0.1f);
+	auto animate = Animate::create(animation);
+	player->runAction(RepeatForever::create(animate));
 	return true;
 }
 
