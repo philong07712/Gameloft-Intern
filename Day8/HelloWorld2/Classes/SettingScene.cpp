73f813cbf71396511a607377722b069b58a78762
Han <philong07712@gmail.com>
2019-12-01 11:06:00 +0700

    Add SettingScene

---
+++ Day8/HelloWorld2/Classes/SettingScene.cpp
@@ -0,0 +1,34 @@
+﻿# include "LogoScene.h"
+# include "HelloWorldScene.h"
+# include "LoadingScene.h"
+# include "MainMenuScene.h"
+# include "SettingScene.h"
+Scene * SettingScene::createScene()
+{
+	return SettingScene::create();
+}
+
+bool SettingScene::init()
+{
+	srand((unsigned)time(0));
+	if (!Scene::init()) {
+		return false;
+	}
+	scheduleUpdate();
+	addBackground();
+	return true;
+}
+
+void SettingScene::addBackground() {
+	auto backgroundSprite = Sprite::create("backgroundCastles.png");
+	auto visibleSize = Director::getInstance()->getVisibleSize();
+	auto originSize = Director::getInstance()->getVisibleOrigin();
+	backgroundSprite->setPosition(Point(visibleSize.width / 2 + originSize.x, visibleSize.height / 2 + originSize.y));
+	addChild(backgroundSprite, 1);
+}
+
+
+void SettingScene::update(float deltaTime)
+{
+
+}
---
+++ Day8/HelloWorld2/Classes/SettingScene.h
@@ -0,0 +1,18 @@
+
+#pragma once
+#ifndef __SETTING_SCENE_H__
+#define __SETTING_SCENE_H__
+#include "ui/CocosGUI.h"
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class SettingScene : public Scene {
+public:
+	static Scene* createScene();
+	virtual bool init(); 
+	void update(float deltaTime);
+	void addBackground();
+	CREATE_FUNC(SettingScene);
+};
+#endif // __SETTING_SCENE_H__
