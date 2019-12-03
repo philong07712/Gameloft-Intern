cb3d3597876a8e3708054fbedb2ec04c2eed20cc
Han <philong07712@gmail.com>
2019-12-03 14:59:58 +0700

    Fix beauty mainMenu

--- Day8/HelloWorld2/Classes/MainMenuScene.cpp
+++ Day8/HelloWorld2/Classes/MainMenuScene.cpp
@@ -37,38 +37,22 @@ void MainMenuScene::addMenu()
 	auto height = visibleSize.height / 2 + originSize.y;
 	// addLogo
 	auto logo = Sprite::create("logo.png");
-	logo->setPosition(Vec2(width, height + 200));
+	logo->setPosition(Vec2(width, height + 150));
 	addChild(logo);
 	// create itemPlay
-	auto playLabel = Label::create("PLAY", "fonts/MarkerFelt.ttf", 40);
-	auto itemPlay = MenuItemLabel::create(playLabel, CC_CALLBACK_1(MainMenuScene::changePlay, this));
-	itemPlay->setPosition(width, height);
 	// create playButton
 	auto playButton = MenuItemImage::create("icons/play_normal.png", "icons/play_pressed.png", CC_CALLBACK_1(MainMenuScene::changePlay, this));
-	playButton->setPosition(itemPlay->getPosition().x - 125, itemPlay->getPosition().y);
-	playButton->setScale(0.5f);
-	// create settingButton
-	auto settingLabel = Label::create("SETTING", "fonts/MarkerFelt.ttf", 40);
-	auto itemSetting = MenuItemLabel::create(settingLabel, CC_CALLBACK_1(MainMenuScene::changeSetting, this));
-	itemSetting->setPosition(itemPlay->getPosition().x, itemPlay->getPosition().y - 100);
+	playButton->setPosition(width, height - 75);
 	// create settingButton
 	auto settingButton = MenuItemImage::create("setting_normal.png", "setting_pressed.png", CC_CALLBACK_1(MainMenuScene::changeSetting, this));
-	settingButton->setPosition(itemSetting->getPosition().x - 125, itemSetting->getPosition().y);
-	// create itemExit
-	auto exitLabel = Label::create("EXIT", "fonts/MarkerFelt.ttf", 40);
-	auto itemExit = MenuItemLabel::create(exitLabel, CC_CALLBACK_1(MainMenuScene::menuCloseCallback, this));
-	itemExit->setPosition(itemSetting->getPosition().x, itemSetting->getPosition().y - 100);
+	settingButton->setPosition(width * 2 - 75, height - 300);
 	// create closeButton
 	auto closeButton = MenuItemImage::create("CloseNormal.png", "CloseSelected.png", CC_CALLBACK_1(MainMenuScene::menuCloseCallback, this));
-	closeButton->setPosition(itemExit->getPosition().x - 125, itemExit->getPosition().y);
-	closeButton->setScale(1.75f);
+	closeButton->setPosition(width, height - 300);
 	// create Menu
 	Vector<MenuItem*> menuItems;
-	menuItems.pushBack(itemPlay);
 	menuItems.pushBack(playButton);
-	menuItems.pushBack(itemExit);
 	menuItems.pushBack(closeButton);
-	menuItems.pushBack(itemSetting);
 	menuItems.pushBack(settingButton);
 	auto myMenu = Menu::createWithArray(menuItems);
 	myMenu->setPosition(0, 0);
